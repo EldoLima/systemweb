fa0e8957685e620c606bb2ba27a001e8e2b5f9b5
Eldo Lima <eldosilva906@gmail.com>
2025-09-29 20:14:37 -0300

    Created project

---
+++ src/main/java/com/example/demo/SystemwebApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemwebApplication.class, args);
+	}
+
+}
